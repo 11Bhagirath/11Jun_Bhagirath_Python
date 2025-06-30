6db9ac9c013c5d3ba9c92b8e1c68f54c58b079ba
11Bhagirath <gajera.bhagirath@gmail.com>
2025-06-30 12:04:13 +0530

    30-june-2025

---
+++ LAB Practice/Module-2 Collection_Type/Dictionary.py
@@ -0,0 +1,12 @@
+stdata={'id':101,'name':'Bhagirath','sub':'python'}
+'''print(stdata)
+print(stdata['name'])
+print(stdata.get('sub'))
+print(stdata.keys())
+print(stdata.values())'''
+
+
+print(stdata)
+stdata['city']='rajkot' # To add a new pair
+stdata['id']=102 #To Update the value
+print(stdata)
--- LAB Practice/Module-2 Collection_Type/Tuple_Userdefined.py
+++ LAB Practice/Module-2 Collection_Type/Tuple_Userdefined.py
@@ -3,8 +3,8 @@ n=int(input("Enter Number Of City List: "))
 
 for i in range(n):
     j=i+1
-    city=(input(f"Enter {j} City Names: "))
+    city=(input(f"Enter {j} City Name: "))
     data.append(city)
 
-print(data)
-    
+print(tuple(data))
+      
