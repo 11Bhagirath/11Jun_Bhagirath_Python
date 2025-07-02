a53962934c644e53ffd0c9e42f7743eac7f483c3
11Bhagirath <gajera.bhagirath@gmail.com>
2025-07-02 17:30:27 +0530

    02-July-2025

---
+++ LAB Practice/Module-2 Collection_Type/Dictionary_User With Hidden Key.py
@@ -0,0 +1,9 @@
+mydic={}
+keys=['id','name','sub']
+
+for i in keys:
+    j=+1
+    x=input(f"Enter {i} Of {j}: ")
+    mydic[i] = x
+    
+print(mydic)
--- LAB Practice/Module-2 Collection_Type/Dictionary_Userdefined.py
+++ LAB Practice/Module-2 Collection_Type/Dictionary_Userdefined.py
@@ -5,8 +5,7 @@ n=int(input("Enter Number Of Pair: "))
 for i in range(n):
     x=int(input(f"Enter {i+1} ID: "))
     y=input(f"Enter {i+1} Name: ")
-    z=input(f"Enter {i+1} City: ")
 
-    mydic[x] = {"Name" : y, "City" : z}
+    mydic[x] = y
     
 print(mydic)
